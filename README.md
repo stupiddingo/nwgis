992d4dfedaac0556c548350548e89b9c0043c7e4
stupiddingo <brent@stupiddingo.com>
2014-02-02 00:28:10 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+Northwest GIS Users Group
+
+Drupal platform with small customizations for the user group, chiefly centralized login, mapping and newsletters.
---
+++ sites/all/themes/nwgis/templates/region--menu.tpl.php
@@ -0,0 +1,7 @@
+<div<?php print $attributes; ?>>
+  <div<?php print $content_attributes; ?>>
+    <nav class="navigation">
+    <?php print $content; ?>
+	</nav>
+  </div>
+</div>
